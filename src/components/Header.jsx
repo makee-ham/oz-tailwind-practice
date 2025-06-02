51fc40c068789ba72eedefd0545ba6cee25bc4fd
Makee <ina0312@naver.com>
2025-06-02 11:32:58 +0900

    index.css의 전역 스타일 중 margin, padding 0 삭제

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
 
   return (
     <header className="flex justify-between items-center py-2.5 px-5 bg-black">
-      <h2>OZ코딩스쿨</h2>
+      <h2 className="text-2xl font-bold">OZ코딩스쿨</h2>
       <ul className="flex justify-center items-center gap-5">
         {menuItems.map((item) => (
           <li key={item} className="list-none font-normal text-[13px]">
