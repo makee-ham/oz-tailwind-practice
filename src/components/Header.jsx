29041060da887068369c70291542a8804db03ec4
Makee <ina0312@naver.com>
2025-06-02 11:11:33 +0900

    Header 컴포넌트 테일윈드화

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,15 @@
 export default function Header() {
+  const menuItems = ["로그인", "회원가입", "내클래스"];
+
   return (
-    <header className="header-container">
+    <header className="flex justify-between items-center py-2.5 px-5 bg-black">
       <h2>OZ코딩스쿨</h2>
-      <ul className="menu-container">
-        <li>로그인</li>
-        <li>회원가입</li>
-        <li>내클래스</li>
+      <ul className="flex justify-center items-center gap-5">
+        {menuItems.map((item) => (
+          <li key={item} className="list-none font-normal text-[13px]">
+            {item}
+          </li>
+        ))}
       </ul>
     </header>
   );
