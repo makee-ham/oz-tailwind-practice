eaf2afef630ef3a1a71f26edef770bc4393e7214
Makee Ham <79068658+makee-ham@users.noreply.github.com>
2025-06-04 17:54:02 +0900

    Update Content.jsx 오타수정

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -4,13 +4,13 @@ export default function Content({ content }) {
       <img
         src={content.img}
         alt={content.title}
-        className="w-[300px] round-[10px] mb-[3px]"
+        className="w-[300px] rounded-[10px] mb-[3px]"
       />
-      <span className="text-xs font-[#d7fa00] border border-[#d7fa00] py-1 px-[5px] rounded-[3px]">
+      <span className="text-xs text-[#d7fa00] border border-[#d7fa00] py-1 px-[5px] rounded-[3px]">
         모집중
       </span>
       <div className="text-lg font-semibold">{content.title}</div>
-      <p className="text-xs font-[rgb(160, 160, 160)]">{content.subtitle}</p>
+      <p className="text-xs text-[rgb(160, 160, 160)]">{content.subtitle}</p>
     </div>
   );
 }
